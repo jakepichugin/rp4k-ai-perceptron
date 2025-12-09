aea8dc67f4b3dd96c6acc082e5f1e3cda2246524
Jake Pichugin <jakepichugin@gmail.com>
2025-12-08 17:39:58 -0800

    improved graphing, points stay visible. Increased neural Net size, removed delay for testing perpouses

--- Perceptron.py
+++ Perceptron.py
@@ -44,13 +44,20 @@ class Perceptron(nn.Module): # Perceptron is a child of nn.module
     def __init__(self):
         super(Perceptron, self).__init__() #override parent constructor
         self.fc1 = nn.Linear(2, 50)
-        self.fcOut = nn.Linear(50, 1)
+        self.fc2 = nn.Linear(50, 50)
+        self.fc3 = nn.Linear(50, 25)
+        self.fcOut = nn.Linear(25, 1)
         #self.fc = nn.Linear(2, 1) # creating a single layer of the neural network with 2 inputs and 1 output
 
     def forward(self, x): # x is the input tensor, that takes both the x and y
         #sending our weights bias through the activation function to get a number between 0 and 1
-        x = torch.sigmoid(self.fc1(x))
-        return torch.sigmoid(self.fcOut(x)) # sigmoid activation function for rapidly moves between 0 and 1
+        z = torch.sigmoid(self.fc1(x))
+        y = torch.sigmoid(self.fc2(z))
+        v = torch.sigmoid(self.fc3(y))
+        # b = torch.sigmoid(self.fc4(v))
+        # n = torch.sigmoid(self.fc5(b))
+        # m = torch.sigmoid(self.fc6(n))
+        return torch.sigmoid(self.fcOut(v)) # sigmoid activation function for rapidly moves between 0 and 1
 
 def train_model():
      # make sure to use model
@@ -74,7 +81,7 @@ def train_model():
         model = Perceptron().to(device)
 
     criterion = nn.MSELoss() # bianary cross-entropy loss || set value to 0 or 1, making a binary output
-    optimizer = optim.SGD(model.parameters(), lr=0.1) # lr is learning rate
+    optimizer = optim.SGD(model.parameters(), lr=0.9) # lr is learning rate
 
 
     #Train on the models on 500 generation
@@ -93,7 +100,7 @@ def train_model():
             plot_decision_boundary(model, position_min, position_max)
             # fig.canvas.draw()
             fig.canvas.flush_events()
-            time.sleep(0.5)
+            # time.sleep(0.5)
 
 
     # plot_decision_boundary(model, position_min, position_max)
@@ -122,7 +129,13 @@ def plot_decision_boundary(m, position_min, position_max):
     #bwr maps 0 to blue, 0.5 to white, 1 to red
     #edgecolor = k for black outline
     #s = 100 sets the size
-    plt.scatter(np.array(points)[:,0], np.array(points)[:,1], c=labels, cmap="RdBu", edgecolors="k", s=100)
+    plt.scatter(np.array(points)[:,0], np.array(points)[:,1], c=labels, cmap="RdBu", alpha=0.5, edgecolors="k", s=200)
+
+    for i, point in enumerate(points):
+        if labels[i] == 0:
+            plt.scatter(point[0], point[1], color='red')
+        else:
+            plt.scatter(point[0], point[1], color='blue')
 
     plt.title("Perceptron Decision Boundary")
     plt.xlim([-1, 1])
